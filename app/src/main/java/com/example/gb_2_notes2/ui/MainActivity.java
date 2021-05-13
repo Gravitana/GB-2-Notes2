7cb7f709d558501f39a9490554dcd6d545fce3a7
gravitana <v.a.grahov@ya.ru>
2021-05-13 17:52:13 +0500

    Drawer

--- app/src/main/java/com/example/gb_2_notes2/ui/MainActivity.java
+++ app/src/main/java/com/example/gb_2_notes2/ui/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.gb_2_notes2.ui;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -11,6 +14,7 @@ import android.widget.Toast;
 
 import com.example.gb_2_notes2.R;
 import com.example.gb_2_notes2.domain.Note;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NotesListFragment.OnNoteListItemClickListener {
 
@@ -30,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements NotesListFragment
     private void initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
 
-//        initDrawer(toolbar);
+        initDrawer(toolbar);
 
         toolbar.setTitle(R.string.app_title);
 
@@ -38,6 +42,27 @@ public class MainActivity extends AppCompatActivity implements NotesListFragment
 //            Toast.makeText(MainActivity.this, "Action!", Toast.LENGTH_SHORT).show();
             int id = item.getItemId();
             return menuItemClicked(id);
+        });
+    }
+
+    private void initDrawer(Toolbar toolbar) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (menuItemClicked(id)) {
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+            return false;
         });
     }
 
