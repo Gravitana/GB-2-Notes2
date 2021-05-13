f44b9f4ce09b0b874720544ed8a95d25b7d0754e
gravitana <v.a.grahov@ya.ru>
2021-05-13 16:25:44 +0500

    список в альбомной раскладке

--- app/src/main/java/com/example/gb_2_notes2/ui/MainActivity.java
+++ app/src/main/java/com/example/gb_2_notes2/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.gb_2_notes2.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -8,9 +10,37 @@ import com.example.gb_2_notes2.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean isLandscape = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        isLandscape = getResources().getBoolean(R.bool.isLandscape);
+
+        initContent();
+    }
+
+    private void initContent() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (isLandscape) {
+            Fragment fragment = fragmentManager.findFragmentById(R.id.list_fragment);
+
+            if (fragment == null) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.list_fragment, new NotesListFragment())
+                        .commit();
+            }
+        } else {
+            Fragment fragment = fragmentManager.findFragmentById(R.id.container);
+
+            if (fragment == null) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, new NotesListFragment())
+                        .commit();
+            }
+        }
     }
 }
